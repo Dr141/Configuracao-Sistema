f88f0bfec534b5ba690676b78f3aa24ef7a8b043
Dr141 <rhemersondr14@gmail.com>
2022-10-20 16:30:03 -0300

    continuar amanhã.

--- Templates/FormPrincipal.cs
+++ Templates/FormPrincipal.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Configuracao.IO;
+using Configuracao.Modelos;
 using System.Windows.Forms;
 
 namespace Configuracao.Templates
 {
     public partial class FormPrincipal : Form
     {
+        private string caminhoArquivo { get; set; }
+        public ManipularXML ler = new ManipularXML();
+
         public FormPrincipal()
         {
             InitializeComponent();
@@ -19,7 +16,53 @@ namespace Configuracao.Templates
 
         private void buttCIP_MouseClick(object sender, MouseEventArgs e)
         {
+            SelecionarArquivo();
+            CIP cip = new CIP();
+            ler.GetDados(cip, caminhoArquivo);
+        }
 
+        private void SelecionarArquivo()
+        {
+            openFileDialog.Multiselect = false;
+            openFileDialog.Title = "Selecionar config";
+
+            openFileDialog.InitialDirectory = @"";
+
+            openFileDialog.Filter = "(*.config)|*.config";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            DialogResult dr = this.openFileDialog.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                caminhoArquivo  = openFileDialog.FileName;
+            }
+        }
+
+        private void buttDADP_MouseClick(object sender, MouseEventArgs e)
+        {
+            SelecionarArquivo();            
+        }
+
+        private void buttLauncher_MouseClick(object sender, MouseEventArgs e)
+        {
+            SelecionarArquivo();
+        }
+
+        private void buttP88_MouseClick(object sender, MouseEventArgs e)
+        {
+            SelecionarArquivo();
+        }
+
+        private void buttSEDIGO_MouseClick(object sender, MouseEventArgs e)
+        {
+            SelecionarArquivo();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            SelecionarArquivo();
         }
     }
 }
